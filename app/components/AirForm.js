48b012a356eae59d5a818aaf65f14d0072c9a463
Yuri <snicksnk@MacBook-Pro-Yuri.local>
2017-03-08 20:47:15 +0400

    Styles for form Labels for form

--- app/components/AirForm.js
+++ app/components/AirForm.js
@@ -2,20 +2,22 @@ import React, { Component, PropTypes } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AirportSelect from './AirportSelectHelper';
 
-export default class AirForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { value: '' };
-    }
+import styles from '../styles/airForm.css';
 
+export default class AirForm extends Component {
     render() {
         const { loadAirPorts, airPortsItems } = this.props;
 
-        return (<div>
-            <input type="number" />
-            <AirportSelect loadAirPorts={loadAirPorts} airPortsItems={airPortsItems} />
-            <AirportSelect loadAirPorts={loadAirPorts} airPortsItems={airPortsItems} />
-        </div>);
+        return (<form className={styles.form}>
+            <label htmlFor="numberOfPassengers" className={styles.label}>Количество пассажиров</label>
+            <input id="numberOfPassengers" type="number" />
+
+            <label htmlFor="departureAirport" className={styles.label}>Аэропорт вылета</label>
+            <AirportSelect id="departureAirport" loadAirPorts={loadAirPorts} airPortsItems={airPortsItems} />
+
+            <label htmlFor="arrivalAirport" className={styles.label}>Аэропорт прибытия</label>
+            <AirportSelect id="arrivalAirport" loadAirPorts={loadAirPorts} airPortsItems={airPortsItems} />
+        </form>);
     }
 }
 
